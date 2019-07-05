182380b7aa707d483e62953c34b6eebc53fb7afc
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-07-04 18:19:13 -0700

    Insert scraped data into db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,52 +3,68 @@ const db = require("../models");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+/**
+ * Scrape a page from https://www.entrepreneur.com/topic/coding and store data into DB
+ * @param {int} pageNumber
+ */
+function scrapePage(pageNumber) {
+    // Get website
+    axios.get(`https://www.entrepreneur.com/topic/coding/${pageNumber}`).then(response => {
+        // Save HTML into $
+        const $ = cheerio.load(response.data);
+        
+        // Iterate through each `.block` div and get article information
+        $(".block").each((i, element) => {
+            // Article titles
+            // console.log($(element).find("h3 a").text().trim());
+            let title = $(element).find("h3 a").text().trim();
+
+            // Article summaries
+            // console.log($(element).find(".deck").text().trim());
+            let summary = $(element).find(".deck").text().trim();
+
+            // Article authors
+            // console.log($(element).find(".name").text().trim());
+            let author = $(element).find(".name").text().trim();
+
+            // Article read times
+            // console.log($(element).find(".readtime").text().trim());
+            let readTime = $(element).find(".readtime").text().trim();
+
+            // Article link
+            // console.log(`https://www.entrepreneur.com${$(element).find("h3 a").attr("href")}`);
+            let link = `https://www.entrepreneur.com${$(element).find("h3 a").attr("href")}`;
+
+            // Article image
+            // console.log($(element).parent().find("img").attr("src").trim());
+            let image = $(element).parent().find("img").attr("src").trim();
+
+            // Assign new article object to articleObj
+            let articleObj = {
+                title: title,
+                summary: summary,
+                author: author,
+                readTime: readTime,
+                link: link,
+                image: image
+            };
+            console.log(articleObj);
+
+            // Add articleObj to Articles collection in DB
+            db.Article.create(articleObj)
+                .then(dbArticle => console.log(dbArticle))
+                .catch(err => console.log(err));
+        });
+    })
+}
+
 // Export API routes
 module.exports = app => {
     // Scrape route
     app.get("/scrape", (req, res) => {
-        // Get website
-        axios.get("https://www.entrepreneur.com/topic/coding/1").then(response => {
-            // Save HTML into $
-            const $ = cheerio.load(response.data);
+        scrapePage(1);
 
-            let articles = {};
-            
-            $(".block").each((i, element) => {
-                // Article titles
-                // console.log($(element).find("h3 a").text().trim());
-
-                // Article summaries
-                // console.log($(element).find(".deck").text().trim());
-
-                // Article authors
-                // console.log($(element).find(".name").text().trim());
-
-                // Article read times
-                // console.log($(element).find(".readtime").text().trim());
-
-                // Article link
-                // console.log(`https://www.entrepreneur.com${$(element).find("h3 a").attr("href")}`);
-                
-                // Article image
-                // console.log($(element).parent().find("img").attr("src").trim());
-            });
-
-            // TODO: Differentiate between title and author
-            // Articles 2 - 4 Title
-            $("#latest .col").each((i, element) => {
-                // console.log($(element).find("a").text().trim());
-            });
-
-            // TODO: Test and put into .each function
-            //       result should be a proper object
-            // db.Article.create(result)
-            //     .then(dbArticle => console.log(dbArticle))
-            //     .catch(err => console.log(err));
-
-            res.send("Scrape Complete");
-        })
-        // res.send("In GET /scrape")
+        res.send("Scrape Complete");
     });
 
     // Get all articles from the database
